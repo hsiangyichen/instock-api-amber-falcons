82c43dc873f20c50e5c60137be68299075d0d555
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-05 17:08:46 -0700

    add method to create an item

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -14,3 +14,50 @@ export async function getAll(req, res) {
     res.status(500).send(`Error getting inventory`);
   }
 }
+export async function createItem(req, res) {
+  const { warehouse_id, item_name, description, category, status, quantity } =
+    req.body;
+  const newItem = {
+    item_name,
+    description,
+    category,
+    status,
+    quantity,
+    warehouse_id,
+  };
+  if (!warehouse_id || !item_name || !description || !category || !status) {
+    return res.status(400).json({
+      message:
+        "Missing properties in the request body. Please include warehouse_id, item_name, description, category, status, and quantity",
+    });
+  }
+  if (typeof quantity !== typeof 1) {
+    console.log(`Quantity is not a number: ${quantity}`);
+    return res.status(400).send(`Error: Quantity is not a number.`);
+  }
+  try {
+    const warehouseInfo = await knex("warehouses")
+      .where({ id: warehouse_id })
+      .first();
+    if (!warehouseInfo) {
+      console.log(`No warehouse found with id ${warehouse_id}.`);
+      return res
+        .status(400)
+        .send(
+          `Unable to add new item. Warehouse with id ${warehouse_id} does not exist in the warehouses table`
+        );
+    }
+  } catch (error) {
+    console.log(`Error finding warehouse with id ${warehouse_id}. ${error}`);
+    return res
+      .status(500)
+      .send(`Unable to add new item. Warehouse id not valid`);
+  }
+  try {
+    const itemId = await knex("inventories").insert(newItem);
+    return res.status(201).send(`${itemId[0]}`);
+  } catch (error) {
+    console.log(`Unable to add an item with this body: ${newItem}. ${error}`);
+    return res.status(500).send(`Unable to add new item`);
+  }
+}
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 import * as inventoryController from "../controllers/inventory-controller.js";
 
 //hit this at : http://localhost:8080/api/inventory
-router.route("/").get(inventoryController.getAll);
+router
+  .route("/")
+  .get(inventoryController.getAll)
+  .post(inventoryController.createItem);
 
 export default router;
