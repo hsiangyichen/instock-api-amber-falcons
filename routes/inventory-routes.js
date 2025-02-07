770df7ba15bc7ec1f7d1b067cfa84e4a7c4a1fbe
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-06 18:36:02 -0800

    feat: Add delete inventory api

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -7,6 +7,9 @@ import * as inventoryController from "../controllers/inventory-controller.js";
 router.route("/").get(inventoryController.getAll);
 
 //hit this at : http://localhost:8080/api/inventory/2
-router.route("/:id").get(inventoryController.getById);
+router
+  .route("/:id")
+  .get(inventoryController.getById)
+  .delete(inventoryController.deleteInventory);
 
 export default router;
