0dfe1b5ece87f3a9c7643f03ec6351409433b7f7
Chandni Melwani <cmelwani92@gmail.com>
2025-02-08 20:15:05 -0500

    Merge develop into feature-af-15, resolve warehouse conflicts

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-<<<<<<< HEAD
 import {
   getAllWarehouses,
   addWarehouse,
   deleteWarehouse,
+  getWarehouseDetails
 } from "../controllers/warehouse-controller.js";
 import { getInventoriesById } from "../controllers/inventory-controller.js";
 
@@ -11,15 +11,8 @@ const router = express.Router();
 
 router.route("/").get(getAllWarehouses).post(addWarehouse);
 
-router.route("/:id").delete(deleteWarehouse);
+router.route("/:id").get(getWarehouseDetails).delete(deleteWarehouse);
 
 router.route("/:id/inventories").get(getInventoriesById);
-=======
-import { getAllWarehouses, getWarehouseDetails } from "../controllers/warehouse-controller.js";
-const router = express.Router();
-
-router.route("/").get(getAllWarehouses);
-router.route("/:id").get(getWarehouseDetails)
->>>>>>> e685132 (added warehouse details in controller and routes)
 
 export default router;
