6aff37c1d8e8a69ea80e431097dc493f98458093
Chandni Melwani <cmelwani92@gmail.com>
2025-02-09 02:02:15 -0500

    created put / edit warehouse fx

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,7 +3,8 @@ import {
   getAllWarehouses,
   addWarehouse,
   deleteWarehouse,
-  getWarehouseDetails
+  getWarehouseDetails,
+  editWarehouse
 } from "../controllers/warehouse-controller.js";
 import { getInventoriesById } from "../controllers/inventory-controller.js";
 
@@ -11,7 +12,7 @@ const router = express.Router();
 
 router.route("/").get(getAllWarehouses).post(addWarehouse);
 
-router.route("/:id").get(getWarehouseDetails).delete(deleteWarehouse);
+router.route("/:id").get(getWarehouseDetails).delete(deleteWarehouse).put(editWarehouse);
 
 router.route("/:id/inventories").get(getInventoriesById);
 
